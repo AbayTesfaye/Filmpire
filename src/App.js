6bd22e334c94c5ce7d84b5589c8af95489096021
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-09 06:04:27 -0700

    Fetch film info from static object

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import { useEffect } from "react";
 import "./App.css";
+import SearchIcon from "./search.svg";
 
 // 2611b215
 const API_URL = "http://www.omdbapi.com?apikey=2611b215";
+
+const Movie1 = {
+  Title: "Spiderman and Grandma",
+  Year: "2009",
+  imdbID: "tt1433184",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg",
+};
 
 function App() {
   const searchMovies = async (title) => {
@@ -14,7 +24,33 @@ function App() {
   useEffect(() => {
     searchMovies("Spiderman");
   }, []);
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <h1>Movie Land</h1>
+      <div className="search">
+        <input
+          placeholder="Search any Movie"
+          value="Spiderman"
+          onClick={() => {}}
+        />
+        <img src={SearchIcon} alt="search" />
+      </div>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{Movie1.Year}</p>
+          </div>
+          <div>
+            <img src={Movie1.Poster} alt={Movie1.title} />
+          </div>
+          <div>
+            <span>{Movie1.Type}</span>
+            <h3>{Movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
